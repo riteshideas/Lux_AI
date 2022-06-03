8a56c847b427d69297ee9ba913526f3b4d86a7c2
riteshideas <ritesh.sathiyamoorthi@gmail.com>
2022-06-03 11:25:36 +0530

    Minor changes

--- intermediate/agent.py
+++ intermediate/agent.py
@@ -12,36 +12,35 @@ import random
 
 DIRECTIONS = Constants.DIRECTIONS
 
+
+
 game_state = None
 build_loc = None
 
-
 build_cart = False
-unit_to_resource_dict = {}
-unit_to_city = {}
 worker_pos = {}
 unit_worker = {}
 unit_cart = {}
+unit_to_city = {}
 
 global_observation = 0
 prev_text = 0
 
 
 
-
-with open("../agent.log", "w") as f:
+with open("../agent.txt", "w") as f:
     f.write("")
 
 def log(text):
     global global_observation
     global prev_text
 
-    with open("../agent.log", "a") as f:
+    with open("../agent.txt", "a") as f:
         if global_observation["step"] > prev_text:
-            f.write(f"\n[{global_observation['step']}] : {text}\n")
+            f.write(f"\n[{global_observation['step']}]: {text}\n")
             prev_text = global_observation["step"]
         else:
-            f.write(f"[{global_observation['step']}] : {text}\n")     
+            f.write(f"[{global_observation['step']}]: {text}\n")     
 
 def get_resource_tiles(game_state, width, height):
     returnLst: list[Cell] = []
@@ -72,7 +71,7 @@ def get_close_city_tile(player, unit):
     closest_city_tile = None
     for k, city in player.cities.items():
         for city_tile in city.citytiles:
-            if city_tile not in unit_to_city:
+            if city_tile not in unit_to_city.values():
                 dist = city_tile.pos.distance_to(unit.pos)
                 if dist < closest_dist:
                     closest_dist = dist
@@ -109,15 +108,19 @@ def translate_direction_to_pos(dir:str, pos, game_state):
     elif dir == "w":
         return game_state.map.get_cell(pos.x, pos.y - 1)
 
+def findMidPoint(locations):
+    pass
+
+def findChunkyResources(resources, loc, radius, percent):
+    pass
 
 def agent(observation, configuration):
     global game_state
     global global_observation
     global build_loc
-    global unit_to_resource_dict
-    global unit_to_city
     global worker_pos
     global unit_worker
+    global unit_to_city
 
 
 
@@ -152,7 +155,7 @@ def agent(observation, configuration):
 
 
     # Returns true if there is enough fuel to build a city tile and last the night
-    enough_fuel = city_fuel > (250 * len(city_tiles))
+    enough_fuel = ((city_fuel + .1) / (len(city_tiles) + .1)) > 300
 
     resource_tiles: list[Cell] = get_resource_tiles(game_state, width, height)
 
@@ -161,7 +164,7 @@ def agent(observation, configuration):
         if w.id in worker_pos:
             worker_pos[w.id].append((w.pos.x, w.pos.y))
         else:
-            worker_pos[w.id] = deque(maxlen=3)
+            worker_pos[w.id] = deque(maxlen = 3)
             worker_pos[w.id].append((w.pos.x, w.pos.y))
 
     #Initalize the unit_worker
@@ -171,18 +174,32 @@ def agent(observation, configuration):
                 "type": None,
                 "unit" : w,
                 "resource": None,
-                "city": None
+                "city": None,
             }
 
+
+
     for w in workers:
-        if w.id not in unit_to_resource_dict:
+
+        '''
+        Worker: 3 modes,
+
+        Starter -> Collect resources and build few cityTiles, would be replaced later
+        Builder -> Stay at CityTile and get trasferred resources from carts
+        Chopper -> Chop wood and send it to the carts
+        '''
+
+
+        unit_worker[w.id]["type"] = "starter"
+        if unit_worker[w.id]["resource"] is None:
             unit_worker[w.id]["resource"] = get_closest_resource_tile(w, resource_tiles, player)
-        if w.id not in unit_to_city:
+        if unit_worker[w.id]["city"] is None:
             unit_worker[w.id]["city"] = get_close_city_tile(player, w)
+            unit_to_city[w.id] = unit_worker[w.id]["city"]
 
 
     build_city = False
-    if (len(workers) + 0.1) / (len(city_tiles) + 0.1) >= 0.8: # The 0.1 to avoid zero division error but still obtain accuracy
+    if (len(workers) + 0.1) / (len(city_tiles) + 0.1) >= 0.7: # The 0.1 to avoid zero division error but still obtain accuracy
         build_city = True
 
 
@@ -205,7 +222,7 @@ def agent(observation, configuration):
             actions.append(unit.move(random.choice(["n", "s", "e", "w"])))
 
 
-        if unit.is_worker() and unit.can_act():
+        if unit.is_worker() and unit.can_act() and unit_worker[unit.id]["type"] == "starter":
 
             # if the unit is a worker and we have space in cargo, lets find the nearest resource tile and try to mine it
             if unit.get_cargo_space_left() > 0:
@@ -218,6 +235,7 @@ def agent(observation, configuration):
 
                 if cell.has_resource():
                     unit_move_dir = unit.pos.direction_to(cell.pos)
+                
                 else:
                     possible_resource_tile = get_closest_resource_tile(unit, resource_tiles, player)
                     unit_worker[unit.id]["resource"] = possible_resource_tile
@@ -227,6 +245,7 @@ def agent(observation, configuration):
                 if possible_resource_tile.resource.amount < 300:
                     log(unit_move_dir)
                     actions.append(unit.move(random.choice(return_removed_list(["n", "s", "w", "e", "c"], unit_move_dir))))
+                
                 else:
                     actions.append(unit.move(unit_move_dir))
 
@@ -239,7 +258,7 @@ def agent(observation, configuration):
                         empty_tile = get_close_city_tile(player, unit)
                         build_loc = find_empty_tile_near(game_state, empty_tile)
 
-                    if build_loc is not None and unit.pos == build_loc.pos:
+                    if build_loc is not None and unit.pos == build_loc.pos and enough_fuel:
                         log("Building city, Yay!")
                         action = unit.build_city()
                         actions.append(action)
@@ -261,9 +280,10 @@ def agent(observation, configuration):
                             if (next_tile.citytile != None):
                                 if next_move_dir  == "n" or next_move_dir == "s":
                                     move_dir = random.choice(["e", "w"])
-
                                 elif next_move_dir  == "e" or next_move_dir == "w":
                                     move_dir = random.choice(["n", "s"])
+                                else:
+                                    log(f"Position: {next_move_dir}")
                             else:
                                 move_dir = unit.pos.direction_to(build_loc.pos)
 
@@ -279,12 +299,10 @@ def agent(observation, configuration):
                             unit_worker[unit.id]["city"] = get_close_city_tile(player, unit)
                             actions.append(unit.move(unit.pos.direction_to(unit_worker[unit.id]["city"].pos)))
 
-
-
                 # if unit is a worker and there is no cargo space left, and we have cities, lets return to them
                 elif len(player.cities) > 0:
                     log("Going back to city")
-                    if unit.id in unit_to_city and unit_worker[unit.id]["city"] in city_tiles:
+                    if unit_worker[unit.id]["city"] is not None  and unit_worker[unit.id]["city"] in city_tiles:
                         actions.append(unit.move(unit.pos.direction_to(unit_worker[unit.id]["city"].pos)))
                     else:
                         unit_worker[unit.id]["city"] = get_close_city_tile(player, unit)
@@ -292,3 +310,4 @@ def agent(observation, configuration):
     # you can add debug annotations using the functions in the annotate object
     # actions.append(annotate.circle(0, 0))
     return actions
+    
